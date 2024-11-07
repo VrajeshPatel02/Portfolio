7e15ed833b019b1b082f5942f02e7f4f660cb44b
VrajeshPatel02 <kevalpatel2990@gmail.com>
2024-11-07 23:25:47 +0530

    draft-1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',  // Ensures static export
+  output: 'export',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
